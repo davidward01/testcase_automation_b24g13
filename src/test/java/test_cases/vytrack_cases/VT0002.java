a4c1fd47841fbd90c2eea4a19a826a26408ed7ff
davidward01 <davidward.info@gmail.com>
2021-11-11 20:54:21 -0500

    Added code that works to VyTrack test 2.  NEEDS REFACTOR

--- src/test/java/test_cases/vytrack_cases/VT0002.java
+++ src/test/java/test_cases/vytrack_cases/VT0002.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import test_cases.utils.VytrackLogin;
 import test_cases.utils.WebDriverFactory;
 
+import javax.swing.plaf.ActionMapUIResource;
 import java.util.Collections;
 import java.util.List;
 
@@ -30,23 +31,41 @@ public class VT0002 {
         driver.navigate().to("https://qa2.vytrack.com/entity/Extend_Entity_Carreservation");
 
         Thread.sleep(3000);
-        driver.findElement(By.linkText("java.util.Random@7c22d4f")).click();
+//        driver.findElement(By.linkText("java.util.Random@7c22d4f")).click();
 
 //        Actions act = new Actions(driver);
 //        act.moveToElement(driver.findElement(By.linkText(" Fleet"))).perform();
 //
 //        driver.findElement(By.xpath("//a[@href='entity/Extend_Entity_Carreservation']")).click();
 //
-       List<WebElement> carsInFleet = driver.findElements(By.xpath("//tr[@class='grid-row row-click-action']"));
+       //List<WebElement> carsInFleet = driver.findElements(By.tagName("tr"));
+        List<WebElement> carsInFleet = driver.findElements(By.xpath("//tr[@class='grid-row row-click-action']"));
+
 
 //        for (WebElement each : carsInFleet) {
 //            System.out.println(each.getText());
 //        }
 
-        System.out.println(carsInFleet);
+//        System.out.println(carsInFleet);
+//        System.out.println(carsInFleet.size());
+        Collections.shuffle(carsInFleet);
+        carsInFleet.get(0).click();
 
-//        Collections.shuffle(carsInFleet);
-//        carsInFleet.get(0).click();
+        String expectedTitle = driver.getTitle();
+
+        if (expectedTitle.contains("Car")){
+            System.out.println("PASS - Vehicle information is visible");
+        } else {
+            System.out.println("FAIL - Vehicle information is not visible");
+        }
+
+
+
+        //WebElement actualElem = driver.findElement(By.xpath("//h5[@class='user-fieldset']"));
+
+//        if (actualElem.isDisplayed()){
+//            System.out.println("nice");
+//        }
 
 
     }
