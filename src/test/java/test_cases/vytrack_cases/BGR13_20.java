d3b0a677cc10af1a4fb24d6c0eaa3fef73775ddb
matthewOldenburg <matthew.r.oldenburg@gmail.com>
2021-11-24 15:52:42 -0700

    BGR13_20 -> automated solution for test BGR13_20 -includes automated screenshot -includes Mert's solution for Refresh button position

---
+++ src/test/java/test_cases/vytrack_cases/BGR13_20.java
@@ -0,0 +1,82 @@
+package test_cases.vytrack_cases;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.WebDriverFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class BGR13_20 {
+    WebDriver driver;
+    @BeforeClass
+    public void setUp(){
+        String url = "https://qa2.vytrack.com/user/login";
+        String expectedTitle = "Login";
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(url);
+        assertEquals(driver.getTitle(), expectedTitle);
+    }
+
+    @Test (priority = 1)
+    public void logIn(){
+        String username = "User36";
+        String password = "UserUser123";
+        String expectedTitle = "Dashboard";
+        WebElement usernameBox = driver.findElement(By.name("_username"));
+        usernameBox.sendKeys(username);
+        WebElement passwordBox = driver.findElement(By.name("_password"));
+        passwordBox.sendKeys(password);
+        WebElement logInButton = driver.findElement(By.id("_submit"));
+        logInButton.click();
+        assertEquals(driver.getTitle(), expectedTitle);
+    }
+
+    @Test (priority = 2)
+    public void navigateToVehiclesPage() throws InterruptedException {
+        Actions actions = new Actions(driver);
+        WebElement fleetTab = driver.findElement(By.xpath("//span[contains(text(),'Fleet')]"));
+        WebElement vehiclesButton = driver.findElement(By.xpath("//span[contains(text(),'Vehicles')]"));
+        actions.moveToElement(fleetTab).perform();
+        actions.moveToElement(vehiclesButton).perform();
+        vehiclesButton.click();
+
+        Thread.sleep(5000);
+
+        //verify correct page opens after clicking vehicles
+        String expectedTitle = "Car - Entities - System - Car - Entities - System";
+        assertEquals(driver.getTitle(), expectedTitle);
+    }
+
+    // automated screenshot of the Vehicles page...stole Mert's solution to find the Refresh button element
+    @Test (priority = 3)
+    public void findPosition() throws IOException {
+        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File(System.getProperty("user.dir")+"\\screenshots\\screenshot.png"));
+
+        WebElement followingResetButton = driver.findElement(By.xpath("//a[@title='Refresh']//following-sibling::a"));
+
+        //checking if following sibling of Refresh button.getText equals "Reset"
+        Assert.assertEquals(followingResetButton.getText(),"Reset", "Something is wrong");
+        System.out.println("Test Passed successfully");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.close();
+    }
+
+}
+
+
+
